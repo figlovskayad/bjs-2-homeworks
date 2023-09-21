0cc193298fcfc3f351e193e9034df2efd9af05a1
netol <figlovskayad@gmail.com>
2023-09-21 19:38:27 +0300

    First task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+	let arr = [];
+	let D = b ** 2 - 4 * a * c;
+	if (D > 0) {
+		let root_1 = (-b + Math.sqrt(D)) / (2 * a);
+		let root_2 = (-b - Math.sqrt(D)) / (2 * a);
+		arr.push(root_1, root_2);
+	} else if (D === 0) {
+		let root = -b / (2 * a)
+		array.push(root);
+	}
+	return arr;
+};
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let P = percent / 100 / 12;
+	let S = amount - contribution;
+	let n = countMonths;
+	let payment = S * (P + (P / (((1 + P) ** n) - 1)));
+	let result = payment * n;
+	return result;
 }
