2ac79bc0cc096b3ce32cabc79072da114a2f3adf
netol <figlovskayad@gmail.com>
2023-10-31 13:28:37 +0300

    Homework  arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
+    if(arr1.length != arr2.length){
+        return false;
+    }
+    return arr1.every((value, index) => {
+        console.log(value, arr2[index]);
+        return value === arr2[index];
+    });
+
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filterUsers = users.filter(user => user.gender === gender);
+    if(filterUsers.length === 0) {
+        return 0;
+    }
+
+    const totalAge = filterUsers.reduce((acc, user) => acc + user.age, 0);
+    const avgAge = totalAge / filterUsers.length;
+    return avgAge;
+
 }
